6e462886c2e56dcf5c7cb633b05a5e0b7186f014
Emma <88465944+emijane@users.noreply.github.com>
2025-11-13 02:45:43 -0500

    updated profile card text

--- portfolio/src/layout/ProfileCard.jsx
+++ portfolio/src/layout/ProfileCard.jsx
@@ -13,7 +13,7 @@ export default function ProfileCard() {
             <img src={icon} alt="Icon" className='w-30 lg:w-35 rounded-full'/>
             <div className='text-left'>
               <h1 className="text-xl lg:text-3xl text-white text-glow">Emma DeNunzio</h1>
-              <p className='text-sm lg:text-lgfont-medium text-pink-200 mt-1'>Frontend Developer | CS @ UF</p>
+              <p className='text-sm lg:text-lgfont-medium text-pink-200 mt-1'>Jr Frontend Developer | CS @ UF</p>
               <div className='flex flex-row gap-3 mt-2'>
                 <Linkedin className='text-white/70 w-5 hover:text-white cursor-pointer' />
                 <Instagram className='text-white/70 w-5 hover:text-white cursor-pointer' />
