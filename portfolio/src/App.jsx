52222783cb6c01fa20dfcc6ee1b58d9af2001916
Emma <88465944+emijane@users.noreply.github.com>
2025-11-13 13:55:45 -0500

    added shooting stars animated background

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,25 +5,34 @@ import ProjectsSection from './layout/ProjectSection';
 import ProfileCard from './layout/ProfileCard';
 import ExperienceSection from './layout/ExperienceSection';
 import SpotifyWidget from './components/SpotifyWidget';
-
+import { StarsBackground } from './components/ui/stars-background';
+import { ShootingStars } from './components/ui/shooting-stars';
 
 function App() {
-  return (
-    <>
-      <div className='flex gap-10 mt-10 justify-center items-center'>
-        <div className='flex flex-col xl:flex-row gap-10'>
-          <div className='flex flex-col justify-center'>
-            <ProfileCard />
-            <SpotifyWidget />
-          </div>
-          <div className='max-w-250'>
-            <ExperienceSection />
-            <ProjectsSection />
-          </div>
+    return (
+        <div className="relative min-h-screen w-full overflow-hidden bg-black text-white">
+
+            {/* ★ Background Layers */}
+            <StarsBackground className="z-0" />
+            <ShootingStars className="z-0" />
+
+            {/* ★ Main Content */}
+            <div className="relative z-10 flex gap-10 mt-10 justify-center items-center">
+                <div className="flex flex-col xl:flex-row gap-10">
+                    <div className="flex flex-col justify-center">
+                        <ProfileCard />
+                        <SpotifyWidget />
+                    </div>
+
+                    <div className="max-w-250">
+                        <ExperienceSection />
+                        <ProjectsSection />
+                    </div>
+                </div>
+            </div>
+
         </div>
-      </div>
-    </>
-  )
+    );
 }
 
-export default App
+export default App;
